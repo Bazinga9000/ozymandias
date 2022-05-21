0470ac011e1f52801accc465214207343528b881
Bazinga9000 <bazonkidonk@gmail.com>
2022-05-21 19:31:35 -0400

    more move combinators

--- src/Mover.hs
+++ src/Mover.hs
@@ -6,11 +6,11 @@ import           Vec
 
 --no moves
 nullMover :: Mover
-nullMover = MkMover {runMover = \_ -> return []}
+nullMover = MkMover {runMover = \_ -> put []}
 
 --only move is a move with no atoms
 emptyMover :: Mover
-emptyMover = MkMover {runMover = \_ -> return [Move []]}
+emptyMover = MkMover {runMover = \_ -> put [Move []]}
 
 --only move is the zero move
 zeroMover :: Mover
@@ -18,21 +18,44 @@ zeroMover = leaper vzero
 
 --mover with a single direction of movement
 leaper :: Vec -> Mover
-leaper v = MkMover {runMover = \_ -> return [Move [Atom v MoveTo]]}
+leaper v = MkMover {runMover = \_ -> put [Move [Atom v MoveTo]]}
 
 --union
 (|+|) :: Mover -> Mover -> Mover
-x |+| y = MkMover {runMover = \b -> liftM2 (++) (runMover x b) (runMover y b)}
+x |+| y = MkMover {runMover = \b -> do
+    m <- liftM2 (++) (runMover x b >> get) (runMover y b >> get)
+    put m
+    }
 
 --mandatory compose, that is "do x then do y"
 (|.|) :: Mover -> Mover -> Mover
 x |.| y = MkMover {runMover = \b -> do
-    xs <- runMover x b
-    put xs
-    ys <- runMover x b
-    return [concatMoves xm ym | xm <- xs, ym <- ys]
+    xs <- runMover x b >> get
+    ys <- runMover x b >> get
+    put [concatMoves xm ym | xm <- xs, ym <- ys]
     }
 
 --optional compose, that is "do x and then you may or may not do y"
 (|.?|) :: Mover -> Mover -> Mover
 a |.?| b = a |.| (emptyMover |+| b)
+
+--filter out moves by some predicate
+filterMv :: Mover -> (Move -> Bool) -> Mover
+filterMv m f = MkMover {runMover = \b -> do
+    mvs <- runMover m b >> get
+    put $ filter f mvs
+    }
+
+--change the type of all moves
+retypeMv :: Mover -> AtomType -> Mover
+retypeMv m t = MkMover {runMover = \b -> do
+    mvs <- runMover m b >> get
+    put $ map (retypeMove t) mvs
+    }
+
+--repeat a move up to n times
+freeN :: Int -> Mover -> Mover
+freeN n m
+    | n < 0 = error "Negative value passed to freeN"
+    | n == 0 = nullMover
+    | n > 0 = m |.?| freeN (n-1) m
--- src/Types.hs
+++ src/Types.hs
@@ -4,17 +4,23 @@ module Types where
 import           Control.Lens
 import           Control.Lens.TH
 import           Control.Monad.RWS
-import           Data.Map
+import qualified Data.Map          as M
 import           Vec
 
 data AtomType = MoveTo | CaptureAt
 
 data Atom = Atom Vec AtomType
 
+retypeAtom :: AtomType -> Atom -> Atom
+retypeAtom t (Atom v _) = Atom v t
+
 type MoveMachine = RWS Pos () [Move]
-newtype Mover = MkMover {runMover :: Board -> MoveMachine [Move]}
+newtype Mover = MkMover {runMover :: Board -> MoveMachine ()}
 
 newtype Move = Move [Atom]
+
+retypeMove :: AtomType -> Move -> Move
+retypeMove t (Move as) = Move $ map (retypeAtom t) as
 
 concatMoves :: Move -> Move -> Move
 concatMoves (Move a) (Move b) = Move $ a ++ b
@@ -22,7 +28,7 @@ concatMoves (Move a) (Move b) = Move $ a ++ b
 newtype Pos = Pos [Int]
 
 newtype Board = Board {
-    _grid :: Map Pos Piece
+    _grid :: M.Map Pos Piece
 }
 
 newtype Piece = Piece {
